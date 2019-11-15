892510e18412c98a7f7c26a3b1f8887de4d87443
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-11-15 21:01:37 +0300

    Init project

---
+++ AnimalClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalClassifier
+//
+//  Created by yauheni prakapenka on 15.11.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
