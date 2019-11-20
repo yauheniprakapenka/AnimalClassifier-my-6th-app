39ffca292bd4f7099750b877b6345e8d000173bf
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-11-20 22:07:55 +0300

    Added Plane class, added ARSCNViewDelegate

--- FaceTracking/FaceTrackingViewController.swift
+++ FaceTracking/FaceTrackingViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 import SceneKit
 import ARKit
 
-class FaceTrackingViewController: UIViewController, ARSCNViewDelegate {
+class FaceTrackingViewController: UIViewController {
+    
+    // MARK: - Input
+    var planes = [Plane]()
     
     // MARK: - Subview
     
@@ -36,35 +39,16 @@ class FaceTrackingViewController: UIViewController, ARSCNViewDelegate {
         super.viewDidLoad()
 
         sceneView.delegate = self
-        sceneView.showsStatistics = true
         sceneView.autoenablesDefaultLighting = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        
         let scene = SCNScene()
-        
-        createBox(in: scene)
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(boxTapped(touch:)))
-        self.sceneView.addGestureRecognizer(gestureRecognizer)
-        
         sceneView.scene = scene
-    }
-    
-    @objc func boxTapped(touch: UITapGestureRecognizer) {
-        let sceneView = touch.view as! SCNView
-        let touchLocation = touch.location(in: sceneView)
-        let touchResults = sceneView.hitTest(touchLocation, options: [:])
-        guard !touchResults.isEmpty, let node = touchResults.first?.node else { return }
-        let boxMaterial = SCNMaterial()
-        boxMaterial.diffuse.contents = UIColor.white
-        boxMaterial.specular.contents = UIColor.red
-        node.geometry?.materials[0] = boxMaterial
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
     }
     
@@ -72,69 +56,6 @@ class FaceTrackingViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillDisappear(animated)
         
         sceneView.session.pause()
-    }
-}
-
-// Action
-
-private extension FaceTrackingViewController {
-    
-    private func createSphere(in scene: SCNScene) {
-        let sphereGeometry = SCNSphere(radius: 0.1)
-        let sphereMaterial = SCNMaterial()
-        sphereMaterial.diffuse.contents = UIImage(named: "555.jpg")
-        // sphereMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 2) - изменить размер текстуры, здесь уменьшили в 2 раза
-        let sphereNode = SCNNode(geometry: sphereGeometry)
-        sphereNode.geometry?.materials = [sphereMaterial]
-        sphereNode.position = SCNVector3(0, 0, -0.7)
-        
-        scene.rootNode.addChildNode(sphereNode)
-    }
-    
-    private func createText(in scene: SCNScene) {
-        // Текст
-        let textGeometry = SCNText(string: "Slipknot", extrusionDepth: 2.0)
-        let textMaterial = SCNMaterial()
-        textMaterial.diffuse.contents = UIColor.red
-        let textNode = SCNNode(geometry: textGeometry)
-        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
-        textNode.geometry?.materials = [textMaterial]
-        textNode.position = SCNVector3(0, 0.2, -1.0)
-        
-        scene.rootNode.addChildNode(textNode)
-    }
-    
-    private func createFigures(in scene: SCNScene) {
-        let array: [SCNGeometry] = [SCNPlane(), SCNSphere(), SCNBox(), SCNPyramid(), SCNTube(), SCNCone(), SCNTorus(), SCNCylinder(), SCNCapsule()]
-        var xCoordinate: Double = 1
-        
-        for geometryShape in array {
-            let node = SCNNode(geometry: geometryShape)
-            
-            let material = SCNMaterial()
-            material.diffuse.contents = UIColor.red
-            
-            node.geometry?.materials = [material]
-            node.scale = SCNVector3(0.1, 0.1, 0.1)
-            
-            node.position = SCNVector3(xCoordinate, 0, -1)
-            xCoordinate -= 0.2
-            
-            scene.rootNode.addChildNode(node)
-        }
-    }
-    
-    private func createBox(in scene: SCNScene) {
-        // Квадрат
-        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-        let boxMaterial = SCNMaterial()
-        boxMaterial.diffuse.contents = UIColor.brown
-        boxMaterial.specular.contents = UIColor.yellow
-        let boxNode = SCNNode(geometry: boxGeometry)
-        boxNode.geometry?.materials = [boxMaterial]
-        boxNode.position = SCNVector3(0, 0, -1.0)
-        
-        scene.rootNode.addChildNode(boxNode)
     }
 }
 
@@ -148,5 +69,24 @@ private extension FaceTrackingViewController {
             sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+}
+
+extension FaceTrackingViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first
+        
+        guard plane != nil else { return }
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
 }
